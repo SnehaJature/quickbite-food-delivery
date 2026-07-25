306d0aab52885f20098580bbf8869c3a27be1548
Sneha Jature <snehajature@Snehas-MacBook-Air.local>
2026-07-25 21:27:06 +0530

    Created Eureka service registry

---
+++ quickbite-backend/quickbite-service-registry/src/test/java/com/quickbite/serviceregistry/QuickbiteServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickbite.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickbiteServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
